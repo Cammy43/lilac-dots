5d6cb67ab14d5ef76ea3084d851e3579a6460034
Cammy43 <112432153+Cammy43@users.noreply.github.com>
2026-08-21 15:50:24 -0400

    Add battery check and mode switching functionality
    This script is called by batCheck.cpp periodically. I use separate scripts because c++'s sleep frees the cpu to do other things. also because popen in c++ is hell

---
+++ config/hypr/lilac/lilacPM/batCheck/batCheck.lua
@@ -0,0 +1,95 @@
+local function notify(str)
+    io.popen("notify-send \"" .. str .. "\" -a 'lilac::lua::batCheck()'").close();
+end;
+
+local function error(str)
+    notify("[FATAL]:" .. str);
+end;
+
+local function readFile(path) -- simple wrapper function for writing to kv
+    local file = assert(io.open(path, "r"));
+    local out = file:read("*all");
+    file:close();
+    return out;
+end;
+
+local function writeFile(path, str) -- simple wrapper function for writing to kv
+    local file, err = io.open(path, "w");
+
+    if not file then
+        error("Error opening file: " .. err);
+        return;
+    end;
+
+    file:write(str);
+    file:close();
+end;
+
+local function getBat() -- gets the battery percentage using dbus. stiched togheter from multiple stackoverflow posts
+    local cmd = "gdbus call --system " .. "--dest org.freedesktop.UPower " ..
+                    "--object-path /org/freedesktop/UPower/devices/DisplayDevice " ..
+                    "--method org.freedesktop.DBus.Properties.Get " .. "org.freedesktop.UPower.Device Percentage";
+
+    local handle = io.popen(cmd);
+    if not handle then
+        local e = "Command " .. cmd .. " failed!";
+        error(e);
+        return nil, "Command " .. cmd .. " failed!";
+    end;
+
+    local result = handle:read("*a");
+    handle:close();
+
+    local percent_str = result:match("<([%d%.]+)>");
+    if percent_str then
+        return math.floor(tonumber(percent_str));
+    else
+        local e ="Failed to extract battery data!";
+        error(e)
+        return nil, e;
+    end;
+end;
+
+local argc = #arg;
+if argc == 0 then
+    error("No arguments provided! Got: " .. tostring(argc) .. " args");
+    return;
+end;
+local batPct, err = getBat();
+if not batPct then
+    error("Error: " .. err);
+    return;
+end;
+
+local isGameMode = readFile("/home/" .. arg[1] .. "/.config/hypr/lilac/kv/gamemode");
+if isGameMode == "1" then
+    
+end;
+
+local currentModePath = "/home/" .. arg[1] .. "/.config/hypr/lilac/kv/lilacPM/currentMode";
+local currentMode = readFile(currentModePath);
+print(currentMode)
+if readFile("/home/" .. arg[1] .. "/.config/hypr/lilac/kv/lilacPM/charging.txt") == "1" then
+    if currentMode ~= "performance" then 
+        notify("Performance mode");
+    end;
+    os.execute("tlpctl performance");
+    writeFile(currentModePath, "performance");
+    
+else
+    if batPct < 15 then
+        if currentMode ~= "powersaver" then
+            notify("Eco mode");
+        end;
+         os.execute("tlpctl power-saver");
+        writeFile(currentModePath, "powersaver");
+    else
+        if currentMode ~= "balanced" then
+            notify("Balanced mode");
+        end;
+        os.execute("tlpctl balanced");
+        writeFile(currentModePath, "balanced");
+        
+    end;
+end;
+
