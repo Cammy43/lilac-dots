1a7067ee0783b3f140e851db27d24b3980459dad
Cammy43 <112432153+Cammy43@users.noreply.github.com>
2026-08-22 03:54:37 -0400

    Add initial configuration for Lilac
    Added configuration file for lilac

---
+++ config/hypr/lilac.lua
@@ -0,0 +1,22 @@
+-------------------------
+---- CONFIGURE LILAC ----
+-------------------------
+
+ -- Choose when the battery level is considered low. 
+low_battery_level = 15
+
+-- If your device has a battery, set this to true
+battery_installed = true
+
+-- If enabled, gaming mode will switch the monitor config to the one specified in ~/config/hypr/gamingMonitors.lua
+gaming_mode_res_switch = true 
+
+-- Pretty self explanitory. Just paste the output of the "whoami" command. Lilac won't work without this.
+username = "cameronv"
+
+default_terminal = "foot"
+default_file_manager = "thunar"
+default_app_launcher = "hyprlauncher"
+desktop_switch_speed = "hi"
+
+debug_mode = false
