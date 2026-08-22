fea141573d95b39000011009909bfc67e67a93f9
Cammy43 <112432153+Cammy43@users.noreply.github.com>
2026-08-22 03:49:48 -0400

    Refactor battery check logic and improve error handling
    Refactor battery check script to improve command execution and error handling. Also, update mode handling based on battery percentage and charging state.

--- config/hypr/lilac/lilacPM/batCheck/batCheck.lua
+++ config/hypr/lilac/lilacPM/batCheck/batCheck.lua
@@ -1,17 +1,26 @@
 local function notify(str)
     io.popen("notify-send \"" .. str .. "\" -a 'lilac::lua::batCheck()'").close();
-end;
+end
 
 local function error(str)
     notify("[FATAL]:" .. str);
-end;
-
+end
+local function runCmd(cmd)
+    local proc rr = io.popen(cmd);
+    if not proc then
+        error("runCmd(): failed to run command ".. cmd);
+        return "nil";
+    end
+    out = proc.read();
+    proc.close();
+    return out;
+end
 local function readFile(path) -- simple wrapper function for writing to kv
     local file = assert(io.open(path, "r"));
     local out = file:read("*all");
     file:close();
     return out;
-end;
+end
 
 local function writeFile(path, str) -- simple wrapper function for writing to kv
     local file, err = io.open(path, "w");
@@ -19,11 +28,11 @@ local function writeFile(path, str) -- simple wrapper function for writing to kv
     if not file then
         error("Error opening file: " .. err);
         return;
-    end;
+    end
 
     file:write(str);
     file:close();
-end;
+end
 
 local function getBat() -- gets the battery percentage using dbus. stiched togheter from multiple stackoverflow posts
     local cmd = "gdbus call --system " .. "--dest org.freedesktop.UPower " ..
@@ -35,7 +44,7 @@ local function getBat() -- gets the battery percentage using dbus. stiched toghe
         local e = "Command " .. cmd .. " failed!";
         error(e);
         return nil, "Command " .. cmd .. " failed!";
-    end;
+    end
 
     local result = handle:read("*a");
     handle:close();
@@ -44,52 +53,56 @@ local function getBat() -- gets the battery percentage using dbus. stiched toghe
     if percent_str then
         return math.floor(tonumber(percent_str));
     else
-        local e ="Failed to extract battery data!";
+        local e = "Failed to extract battery data!";
         error(e)
         return nil, e;
-    end;
-end;
+    end
+end
 
 local argc = #arg;
 if argc == 0 then
     error("No arguments provided! Got: " .. tostring(argc) .. " args");
     return;
-end;
+end
+
 local batPct, err = getBat();
 if not batPct then
     error("Error: " .. err);
     return;
-end;
-
-local isGameMode = readFile("/home/" .. arg[1] .. "/.config/hypr/lilac/kv/gamemode");
-if isGameMode == "1" then
-    
-end;
-
-local currentModePath = "/home/" .. arg[1] .. "/.config/hypr/lilac/kv/lilacPM/currentMode";
+end
+local lowBatLevel = tonumber(readFile("/home/" .. arg[1] .. "/.config/hypr/lilac/kv/lilacPM/lowbatlevel"));
+local currentModePath = "/home/" .. arg[1] .. "/.config/hypr/lilac/kv/lilacPM/currentmode";
+local isGameMode = readFile("/home/" .. arg[1] .. "/.config/hypr/lilac/kv/gamingmode/state");
+local lpmOpenPath = "/home/" .. arg[1] .. "/.config/hypr/lilac/kv/lilacPM/lpmopen";
 local currentMode = readFile(currentModePath);
-print(currentMode)
-if readFile("/home/" .. arg[1] .. "/.config/hypr/lilac/kv/lilacPM/charging.txt") == "1" then
-    if currentMode ~= "performance" then 
-        notify("Performance mode");
-    end;
-    os.execute("tlpctl performance");
-    writeFile(currentModePath, "performance");
-    
-else
-    if batPct < 15 then
-        if currentMode ~= "powersaver" then
-            notify("Eco mode");
-        end;
-         os.execute("tlpctl power-saver");
-        writeFile(currentModePath, "powersaver");
-    else
-        if currentMode ~= "balanced" then
-            notify("Balanced mode");
-        end;
-        os.execute("tlpctl balanced");
-        writeFile(currentModePath, "balanced");
-        
-    end;
-end;
+if isGameMode == "0" then
+    if readFile("/home/" .. arg[1] .. "/.config/hypr/lilac/kv/lilacPM/charging.txt") == "1" then
+        if currentMode ~= "performance" then
+            notify("Performance mode");
+        end
+        os.execute("tlpctl performance");
+        writeFile(lpmOpenPath,"0");
+        writeFile(currentModePath, "performance");
 
+    else
+        if batPct < lowBatLevel then
+            if currentMode ~= "power-saver" then
+                 notify("Eco mode");
+            end
+            if readFile(lpmOpenPath) ~= "1" then
+                notify("Low battery! " .. tostring(batPct) .. "% remaining.");
+                writeFile(lpmOpenPath,"1");
+            end
+            os.execute("tlpctl power-saver");
+            writeFile(currentModePath, "power-saver");
+        else
+            if currentMode ~= "balanced" then
+                notify("Balanced mode");
+            end
+            writeFile(lpmOpenPath,"0");
+            os.execute("tlpctl balanced");
+            writeFile(currentModePath, "balanced");
+
+        end
+    end
+end
