395e31680fd429e9901e6678aadd0cf4d686d18b
Cammy43 <112432153+Cammy43@users.noreply.github.com>
2026-08-24 12:56:56 -0400

    Fix syntax error in autostart.lua

--- config/hypr/autostart.lua
+++ config/hypr/autostart.lua
@@ -1,6 +1,5 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
-
 hl.on("hyprland.start", function()
     -- put your autostart things here:
     -- example: "Say Welcome!" when hyprland starts:
