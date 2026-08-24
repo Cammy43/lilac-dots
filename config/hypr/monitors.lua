54a36e54210beee25bfdf15b67bed962efc65f39
Cammy43 <112432153+Cammy43@users.noreply.github.com>
2026-08-24 12:34:09 -0400

    Add initial monitor configuration in monitors.lua

---
+++ config/hypr/monitors.lua
@@ -0,0 +1,10 @@
+---- MONITORS ----
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+
+hl.monitor({
+    output   = "",
+    mode     = "auto@auto",
+    position = "auto",
+    scale    = "auto",
+})
