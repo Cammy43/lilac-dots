269fd4b954376998740f22898c50004409cfb777
Cammy43 <112432153+Cammy43@users.noreply.github.com>
2026-08-24 12:42:49 -0400

    Improve package manager detection in install.sh
    Refactor package manager detection and installation commands for various Linux distributions. Add support for Debian, Ubuntu, and NixOS.
    It still doesn't work though

--- install.sh
+++ install.sh
@@ -1,31 +1,58 @@
 #!/bin/bash
+#shits still broken lol
+tput setaf 5
 echo "Initialising..."
-
-get_package_manager() {
-    if command -v apt-get &>/dev/null; then
-        manager= "apt-get"
-    elif command -v dnf &>/dev/null; then
-         manager= "dnf"
-    elif command -v yum &>/dev/null; then
-         manager= "yum"
-    elif command -v pacman &>/dev/null; then
-         manager= "pacman"
-    elif command -v zypper &>/dev/null; then
-         manager= "zypper"
-    elif command -v apk &>/dev/null; then
-         manager= "apk"
-    elif command -v brew &>/dev/null; then
-         manager= "brew"
-    else
-         manager= "unknown"
-        return 1
-    fi
-}
-
-if ! command -v fish &> /dev/null
-then
-    echo "[ERR]  : Some dependancies are not installed! Installing them now."
-    if ! command -v git &> /dev/null
+devMode=0
+if ["$0" = "dev"]; then
+    devMode=1
 fi
 
 
+if [ "$distroName" = "Debian GNU/Linux" ]; then
+    packageManager="apt"
+    installCmd="sudo apt install -y"
+    gitInstall="sudo apt install -y git"
+    distroDir="$HOME/$Distro"
+    
+    elif [ "$distroName" = "Ubuntu" ]; then
+    p ackageManager="apt"
+    installCmd="sudo apt install -y"
+    gitInstall="sudo apt install -y git"
+    
+    elif command -v pacman &> /dev/null; then
+    packageManager="pacman"
+    installCmd="sudo pacman -S --noconfirm"
+    gitInstall="sudo pacman -S git --noconfirm"
+    distroDir="$HOME/$Distro"
+    
+    elif command -v dnf &> /dev/null; then
+    packageManager="dnf"
+    installCmd="sudo dnf install -y"
+    gitInstall="sudo dnf install -y git"
+    distroDir="$HOME/$Distro"
+    
+    elif command -v zypper &> /dev/null; then
+    packageManager="zypper"
+    installCmd="sudo zypper install -y"
+    gitInstall="sudo zypper install -y git"
+    distroDir="$HOME/$Distro"
+    
+    elif [ "$distroName" = "NixOS" ]; then
+    packageManager="nix"
+    installCmd="nix-shell"
+    gitInstall="nix-shell -p git curl pciutils"
+    
+    distroDir="$HOME/$Distro"
+else
+    echo "${ERROR} $distroName isn't a supported distro. Sorry."
+    exit 1
+fi
+
+echo "[INFO] : Detected distro: $distroName"
+
+if ! command -v fish &> /dev/null
+then
+    echo "[WARN] : fish not installed."
+    bash -c "$installCmd fish"
+fi
+fish install.fish $devMode
