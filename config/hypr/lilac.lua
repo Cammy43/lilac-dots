4ecd1b92d2a4830f2546609cdec8c80679bcdabc
Cammy43 <112432153+Cammy43@users.noreply.github.com>
2026-08-24 12:20:05 -0400

    Update power profiles and gaming mode settings

--- config/hypr/lilac.lua
+++ config/hypr/lilac.lua
@@ -5,7 +5,7 @@
  -- Choose when the battery level is considered low. 
 low_battery_level = 15
 power_profile_while_charging = "performance"
-power_profile_on_battery = "power-saver"
+power_profile_on_battery = "balanced"
 power_profile_on_low_battery = "power-saver"
 -- If you are using a laptop, set this to true
 laptop = true
@@ -13,13 +13,13 @@ laptop = true
 -- If enabled, gaming mode will switch the monitor config to the one specified in ~/config/hypr/gamingMonitors.lua
 gaming_mode_res_switch = true
 -- If this is enabled noctalia and other fancy features will be disabled for maximum speed
-gaming_mode_ultra_pro_max = true
+gaming_mode_ultra_pro_max = false
 -- Just paste the output of the "whoami" command if you don't know your username somehow. Lilac won't work without this.
 username = "user"
 
 default_terminal = "foot"
 default_file_manager = "thunar"
 default_app_launcher = "hyprlauncher"
-desktop_switch_speed = "hi"
+desktop_switch_speed = 5
 
 debug_mode = false
