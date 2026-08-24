bba8b8f08b396a555237d9708aa7a5847c4bc2c4
Cammy43 <112432153+Cammy43@users.noreply.github.com>
2026-08-24 12:01:27 -0400

    Create gamingMonitors.lua for gaming mode

---
+++ config/hypr/gamingMonitors.lua
@@ -0,0 +1,13 @@
+---- MONITORS ----
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/ to learn how to configure this.
+
+-- Choose your monitor config for gaming. Lowering your resolution can 
+-- significantly increase fps, but sacrifices visual quality.
+
+hl.monitor({
+    output   = "",
+    mode     = "1152x648@60",
+    position = "auto",
+    scale    = "0.75",
+})
